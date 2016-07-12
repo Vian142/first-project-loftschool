6899950d4606b3df882c52ff9198e2ec9ff747c8
Vian142 <vian142@gmail.com>
2016-07-12 03:42:27 +0300

    Ипсправлен Gulp. Добавлен log-ошибок SASS.Против вылетов.

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('jade', function() {
 
 gulp.task('sass', function(){
 	return gulp.src('app/sass/*.+(sass|scss)')
-	.pipe(sass())
+	.pipe(sass().on('error', sass.logError))
 	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.reload({stream:true}))
